9460ce42865235b273b81d5175edf4812629801a
thavao <othavioact@gmail.com>
2024-04-18 16:04:43 -0300

    Add project files.

---
+++ Laco-Repeticao/Program.cs
@@ -0,0 +1,44 @@
+﻿int valorInicial = 0, max = 0, auxiliar = 0, contador = 0;
+
+Console.Write("Digite o valor inicial: ");
+valorInicial = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Digite o quanto quer incrementar");
+max = int.Parse(Console.ReadLine());
+
+
+Console.WriteLine("For");
+
+auxiliar = valorInicial;
+
+for (contador = 0; contador <= max; contador++)
+{
+    Console.WriteLine(auxiliar);
+    auxiliar++;
+}
+
+Console.WriteLine("Do While");
+
+auxiliar = valorInicial;
+contador = 0;
+
+do
+{
+    Console.WriteLine(auxiliar);
+    auxiliar++;
+    contador++;
+
+} while (contador <= max);
+
+
+Console.WriteLine("While");
+
+auxiliar = valorInicial;
+contador = 0;
+
+while (contador <= max)
+{
+    Console.WriteLine(auxiliar);
+    auxiliar++;
+    contador++;
+}
